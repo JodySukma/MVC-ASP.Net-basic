0a09d1f3ee30bde06fe9ddbaa619ce12aa5f3dd5
lJodyl <zody.kyozue@gmail.com>
2019-04-22 15:14:59 +0700

    first commit

---
+++ SoalKe-1/Controllers/KalkulatorController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SoalKe_1.Models;
+
+namespace SoalKe_1.Controllers
+{
+    public class KalkulatorController : Controller
+    {
+        [HttpGet]
+        public IActionResult Kalkulator()
+        {
+            KalkulatorModel kal = new KalkulatorModel();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Kalkulator(KalkulatorModel kal)
+        {
+            return View(kal);  
+        }
+
+        //public IActionResult Operator()
+        //{
+        //    Operator op = new Operator();
+        //    op.Nilai1
+        //    return ;
+        //}
+    }
+}
---
+++ SoalKe-1/Models/KalkulatorModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoalKe_1.Models
+{
+    public class KalkulatorModel
+    {
+        //private double _angka1;
+        //private double _angka2;
+
+        //private string _operator;
+
+        //public double Angka1 { get => _angka1; set => _angka1 = 0; }
+        //public double Angka2 { get => _angka2; set => _angka2 = 0; }
+        public double Angka1 { get; set; } = 0;
+        public double Angka2 { get; set; } = 0;
+
+        public string Operator { get; set; } = "+";
+
+        public double Hasil
+        {
+            get {
+                if (Operator == "+")
+                    return Angka1 + Angka2;
+                else if (Operator == "-")
+                    return Angka1 - Angka2;
+                else if (Operator == "*")
+                    return Angka1 * Angka2;
+                else
+                    return Angka1 / Angka2;
+            }
+        }
+    }
+}
